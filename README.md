e1c926aa4b0e6a0f273ecfd61e937b82f363723a
prathimaautomation <ramagiri.prathima@gmail.com>
2021-06-29 11:36:45 +0100

    added all the program files

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Building Python Packages
+## What is Python Package and benefits
+### Structure
+
+- step 1 ` create an app folder`
+- step 2 inside app folder ` __init__.py` empty - to initiate our package
+- step 3 inside app folder `fizzbuzz.py` to add our functionality
+- step 4 create `program.py` on dir level - to use as our 
+- `setup.py` on a dir level - to describe our module details such as versioon, author and contact details
+
+```python
+building_python_package
+--app
+--- __init__.py
+--- fizzbuzz.py
+program.py
+setup.py
+```
---
+++ app/fizzbuzz.py
@@ -0,0 +1,26 @@
+class Fizzbuzz:
+
+    def __init__(self, start_of_range, end_of_range):
+        self.fizzrange = range(start_of_range, end_of_range)
+        self.fizzbuzz_list = []
+        self._fizzbuzz_iterator()
+
+    def _divisible_by(self, num1, num2):
+        if num1 % num2 == 0:
+            return True
+        else:
+            return False
+
+    def _fizzbuzz_iterator(self):
+
+        for num in self.fizzrange:
+            if self._divisible_by(num, 15):
+                self.fizzbuzz_list.append("fizzbuzz")
+            elif self._divisible_by(num, 5):
+                self.fizzbuzz_list.append("buzz")
+            elif self._divisible_by(num, 3):
+                self.fizzbuzz_list.append("fizz")
+            else:
+                self.fizzbuzz_list.append(num)
+
+
